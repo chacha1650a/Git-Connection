cc148194de9ddbc98c8ff5dec443fb51f85b66eb
chacha1650a <01058797023a@gmail.com>
2026-08-31 10:45:55 +0900

    4번째 커밋(파일 구조 분리)

--- simple_code/calculator.py
+++ simple_code/calculator.py
@@ -6,7 +6,7 @@ val1 = int(input())
 val2 = int(input())
 
 print("---------------------------")
-print("어떤 연산을 선택 하시겠습니까?")
+print("어떤 사칙연산을 선택 하시겠습니까?")
 print("1. 더하기\n2. 빼기\n3. 곱하기\n4. 나누기")
 
 calType = int(input())
